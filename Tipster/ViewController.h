2dac4c199d4227413b395756d350445f9155d6a1
Sandra Roman Rivera <sroman98@fb.com>
2019-06-25 13:55:41 -0700

    Initial Commit

---
+++ Tipster/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tipster
+//
+//  Created by sroman98 on 6/25/19.
+//  Copyright © 2019 sroman98. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
