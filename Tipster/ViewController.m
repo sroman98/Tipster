f6fa4fdc77ada5f17c9888bb4a5ed99da11e269d
Sandra Roman Rivera <sroman98@fb.com>
2019-06-25 23:15:28 -0700

    Improve animations.

--- Tipster/ViewController.m
+++ Tipster/ViewController.m
@@ -13,6 +13,8 @@
 @property (weak, nonatomic) IBOutlet UITextField *billField;
 @property (weak, nonatomic) IBOutlet UILabel *tipLabel;
 @property (weak, nonatomic) IBOutlet UILabel *totalLabel;
+@property (weak, nonatomic) IBOutlet UIView *grayLine;
+@property (weak, nonatomic) IBOutlet UILabel *dollarLabel;
 
 
 @end
@@ -53,20 +55,33 @@
     
     [UIView animateWithDuration:0.2 animations:^{
         self.billField.frame = CGRectMake(self.billField.frame.origin.x, self.billField.frame.origin.y + 30, self.billField.frame.size.width, self.billField.frame.size.height);
+        self.dollarLabel.frame = CGRectMake(self.dollarLabel.frame.origin.x, self.dollarLabel.frame.origin.y + 30, self.dollarLabel.frame.size.width, self.dollarLabel.frame.size.height);
+        self.grayLine.frame = CGRectMake(self.grayLine.frame.origin.x, self.grayLine.frame.origin.y - 80, self.grayLine.frame.size.width, self.grayLine.frame.size.height);
+        
         self.tipLabel.alpha = 0;
+        self.totalLabel.alpha = 0;
     }];
 }
 - (IBAction)onEditingEnd:(id)sender {
     CGRect newFrame = self.billField.frame;
     newFrame.origin.y -= 30;
     
+    CGRect newFrameDollar = self.dollarLabel.frame;
+    newFrameDollar.origin.y -= 30;
+    
+    CGRect newFrameLine = self.grayLine.frame;
+    newFrameLine.origin.y += 80;
+    
     [UIView animateWithDuration:0.2 animations:^{
         self.billField.frame = newFrame;
+        self.dollarLabel.frame = newFrameDollar;
+        self.grayLine.frame = newFrameLine;
         
     }];
     
     [UIView animateWithDuration:1 animations:^{
         self.tipLabel.alpha = 1;
+        self.totalLabel.alpha = 1;
     }];
     
 }
@@ -84,6 +99,16 @@
     
     self.tipLabel.text = [NSString stringWithFormat:@"$%.2f", tip];
     self.totalLabel.text = [NSString stringWithFormat:@"$%.2f", total];
+    
+    self.tipLabel.alpha = 0;
+    self.totalLabel.alpha = 0;
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [UIView animateWithDuration:0.8 animations:^{
+        self.tipLabel.alpha = 1;
+        self.totalLabel.alpha = 1;
+    }];
 }
 
 @end
